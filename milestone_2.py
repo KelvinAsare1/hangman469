6ac6ffa992d492899f5462f1e3ab344ff8686fcf
KelvinAsare1 <148376344+KelvinAsare1@users.noreply.github.com>
2023-12-10 11:42:23 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,6 @@
-Fruits = [watermelon, banana, orange, mango, grapes]
-print(List)
+Import random
+word_list = [watermelon, banana, orange, mango, grapes]
+def choose_word():
+    word_list = ["watermelon", "banana", "orange", "mango", "grapes"]
+    return random.choice(word_list)
+
