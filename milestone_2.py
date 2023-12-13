7b3d3b2a1762a428e1f5e1880d0825cdee2985c7
KelvinAsare1 <148376344+KelvinAsare1@users.noreply.github.com>
2023-12-13 21:17:21 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,11 +1,10 @@
 import random
-word_list = [watermelon, banana, orange, mango, grapes]
 def choose_word():
     word_list = ['watermelon', 'banana', 'orange', 'mango', 'grapes']
     return random.choice(word_list)
-word = random.choice(word_list)
+word = choose_word()
 guess = input("Enter a single letter")
-if len(guess) = 1 and isalpha(guess),
+if len(guess) == 1 and guess.isalpha():
     print("Good Guess!")
 else:
     print"(Oops! That is not a valid input.")
