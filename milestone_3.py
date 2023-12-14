d9afea96aaf01855f309916dc8d926e0e2a2974a
KelvinAsare1 <148376344+KelvinAsare1@users.noreply.github.com>
2023-12-14 18:45:13 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,1 +1,16 @@
-while 
+def check_guess(guess).lower
+
+def ask_for_input():
+    while True:
+      guess = input("Enter a single letter")
+      if len(guess) == 1 and guess.isalpha():
+        print("Good Guess!")
+        break
+      else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+      if guess in word:
+          print("Good Guess" guess, "is in the word")
+      else:
+          print("Sorry" guess, "is not in the word. Try again.")
+
+  ask_for_input()
